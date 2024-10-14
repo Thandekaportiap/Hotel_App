64806a24b40b643c49f7fb28205ea309bf5e982e
Thandekaportiap <thandekaportiap05@gmail.com>
2024-10-14 16:01:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import RegisterPage from './UserPages/RegisterPage'
+
+function App() {
+ 
+
+  return (
+    <>
+     <RegisterPage/>
+    </>
+  )
+}
+
+export default App
