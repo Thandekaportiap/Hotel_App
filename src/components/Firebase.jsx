e42e0b60c832c7a4f7ddfca85854bf6e16c33260
Thandekaportiap <thandekaportiap05@gmail.com>
2024-10-15 12:20:22 +0200

    feat: firebase set up

---
+++ src/components/Firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXEpP7ik5GuWQ2KK-stxJTkw79pQZ8KAk",
+  authDomain: "awbooking-164e5.firebaseapp.com",
+  projectId: "awbooking-164e5",
+  storageBucket: "awbooking-164e5.appspot.com",
+  messagingSenderId: "768114288072",
+  appId: "1:768114288072:web:203d920e0b1d07df400cfd",
+  measurementId: "G-44DC21TW3G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,20 +6,21 @@ const Logo = require('../assets/Logo.png');
 
 const Navbar = ({ id, onLogout }) => {
     const [openNav, setOpenNav] = useState(true);
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const dropdownRef = useRef(null); 
+    const [loginDropdownOpen, setLoginDropdownOpen] = useState(false);
+    const [registerDropdownOpen, setRegisterDropdownOpen] = useState(false);
+    const loginDropdownRef = useRef(null);
+    const registerDropdownRef = useRef(null);
 
     const ToggleNavBar = () => {
         setOpenNav(!openNav);
     };
 
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
-    };
-
     const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-            setDropdownOpen(false);
+        if (loginDropdownRef.current && !loginDropdownRef.current.contains(event.target)) {
+            setLoginDropdownOpen(false);
+        }
+        if (registerDropdownRef.current && !registerDropdownRef.current.contains(event.target)) {
+            setRegisterDropdownOpen(false);
         }
     };
 
@@ -32,84 +33,55 @@ const Navbar = ({ id, onLogout }) => {
 
     return (
         <>
-            {/* Main Navigation Bar */}
-            <nav className='border bg-[#003060] flex justify-between items-center h-20 mx-auto px-5 text-[#68bbe3]'>
+            <nav className='border bg-[#003060] flex justify-between items-center h-20 mx-auto px-5 text-[#68bbe3] t'>
                 <img src={Logo} alt="Logo" className='h-12 w-16' />
-                
-                {/* Desktop Navigation Links */}
-                <ul className='hidden md:flex space-x-6 text-xl font-semibold'>
+
+                <ul className='hidden md:flex space-x-6 text-2xl font-semibold'>
                     <li>
-                        <NavLink to="/" 
-                            className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}
-                        >
-                            Home
-                        </NavLink>
+                        <NavLink to="/" className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/accommodations" 
-                            className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}
-                        >
-                            Accommodations
-                        </NavLink>
+                        <NavLink to="/accommodations" className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}>Accommodations</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/about" 
-                            className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}
-                        >
-                            About Us
-                        </NavLink>
+                        <NavLink to="/about" className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}>About Us</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contactus" 
-                            className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}
-                        >
-                            Contact Us
-                        </NavLink>
+                        <NavLink to="/contactus" className={({ isActive }) => (isActive ? 'text-[#68bbe3] active:bg-[#003060]' : 'hover:text-[#68bbe3]')}>Contact Us</NavLink>
                     </li>
                 </ul>
 
-                {/* Desktop Buttons */}
                 <div className='hidden space-x-4 md:flex'>
                     {id ? (
-                        <button onClick={onLogout} className="bg-[#68bbe3] px-4 py-2 rounded text-[black]">
-                            Logout
-                        </button>
+                        <button onClick={onLogout} className="bg-[#68bbe3] px-4 py-2 rounded text-[black]">Logout</button>
                     ) : (
                         <>
-                            <div className='relative' ref={dropdownRef}>
-                                <button className='border border-[#68bbe3] text-[#68bbe3] py-2 hover:bg-[#68bbe3] hover:text-white px-4 font-bold rounded-md' onClick={toggleDropdown}>
+                            <div className='relative' ref={loginDropdownRef}>
+                                <button className='border border-[#68bbe3] text-[#68bbe3] py-2 hover:bg-[#68bbe3] hover:text-white px-4 font-bold rounded-md' onClick={() => setLoginDropdownOpen(!loginDropdownOpen)}>
                                     Login
                                 </button>
-                                {dropdownOpen && (
+                                {loginDropdownOpen && (
                                     <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-10">
                                         <Link to="/login/customer">
-                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setDropdownOpen(false)}>
-                                                LogIn as Customer
-                                            </button>
+                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setLoginDropdownOpen(false)}>LogIn as Customer</button>
                                         </Link>
                                         <Link to="/login/admin">
-                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setDropdownOpen(false)}>
-                                                LogIn as Admin
-                                            </button>
+                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setLoginDropdownOpen(false)}>LogIn as Admin</button>
                                         </Link>
                                     </div>
                                 )}
                             </div>
-                            <div className='relative' ref={dropdownRef}>
-                                <button className='border border-[#68bbe3] text-[#68bbe3] py-2 hover:bg-[#68bbe3] hover:text-white px-4 font-bold rounded-md' onClick={toggleDropdown}>
+                            <div className='relative' ref={registerDropdownRef}>
+                                <button className='border border-[#68bbe3] text-[#68bbe3] py-2 hover:bg-[#68bbe3] hover:text-white px-4 font-bold rounded-md' onClick={() => setRegisterDropdownOpen(!registerDropdownOpen)}>
                                     Register
                                 </button>
-                                {dropdownOpen && (
+                                {registerDropdownOpen && (
                                     <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-10">
                                         <Link to="/register/customer">
-                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setDropdownOpen(false)}>
-                                                Register as Customer
-                                            </button>
+                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setRegisterDropdownOpen(false)}>Register as Customer</button>
                                         </Link>
                                         <Link to="/register/admin">
-                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setDropdownOpen(false)}>
-                                                Register as Admin
-                                            </button>
+                                            <button className='w-full text-left px-4 py-2 hover:bg-[#68bbe3] hover:text-white' onClick={() => setRegisterDropdownOpen(false)}>Register as Admin</button>
                                         </Link>
                                     </div>
                                 )}
@@ -118,12 +90,10 @@ const Navbar = ({ id, onLogout }) => {
                     )}
                 </div>
 
-                {/* Hamburger Menu Icon for Mobile */}
                 <div className='fixed md:hidden right-6' onClick={ToggleNavBar}>
                     {!openNav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
                 </div>
 
-                {/* Mobile Navigation Menu */}
                 <div className={!openNav ? 'left-0 fixed top-0 w-[60%] bg-black h-full block pl-4 pt-4 ease-in-out duration-500 md:hidden' : "fixed left-[100%] ease-in-out duration-500"}>
                     <h1 className='text-[27px] font-bold'>Accommodations</h1>
                     <ul className='block pt-8 space-y-4'>
@@ -131,7 +101,7 @@ const Navbar = ({ id, onLogout }) => {
                             <NavLink to="/" className={({ isActive }) => (isActive ? 'text-[#68bbe3]' : '')}>Home</NavLink>
                         </li>
                         <li className='border-b border-[#68bbe3]'>
-                            <NavLink to="/shoppingList" className={({ isActive }) => (isActive ? 'text-[#68bbe3]' : '')}>Accommodations</NavLink>
+                            <NavLink to="/accommodations" className={({ isActive }) => (isActive ? 'text-[#68bbe3]' : '')}>Accommodations</NavLink>
                         </li>
                         <li className='border-b border-[#68bbe3]'>
                             <NavLink to="/about" className={({ isActive }) => (isActive ? 'text-[#68bbe3]' : '')}>About Us</NavLink>
@@ -144,10 +114,10 @@ const Navbar = ({ id, onLogout }) => {
                         <button onClick={onLogout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
                     ) : (
                         <div className='block pt-5 space-y-4'>
-                            <Link to='/Login'>
+                            <Link to='/login'>
                                 <button className='border border-[#68bbe3] hover:bg-[#68bbe3] hover:text-white bg-violet-200 w-full py-2 text-[black] font-bold rounded-md block'>Login</button>
                             </Link>
-                            <Link to="/Register">
+                            <Link to="/register">
                                 <button className='border border-[#68bbe3] hover:bg-[#68bbe3] hover:text-white bg-violet-200 w-full py-2 text-[black] font-bold rounded-md'>Register</button>
                             </Link>
                         </div>
