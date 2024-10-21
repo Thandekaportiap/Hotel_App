6e69f6f3d680914e3c548dc22d136e471df36c7b
Thandekaportiap <thandekaportiap05@gmail.com>
2024-10-21 13:06:47 +0200

    feat: review page added

--- src/UserPages/BookingsHistory.jsx
+++ src/UserPages/BookingsHistory.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import BookingsList from '../components/BookingsList';
+import { selectUserId } from '../features/UsersSlice';
+
 
 const BookingsHistory = () => {
+  
+    const userId = useSelector(selectUserId); 
+
   return (
     <div>
-      Bookings History
+      <h1 className="font-serif text-4xl md:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#003060] to-[#68bbe3] p-2 my-6" >Your Bookings</h1>
+      <BookingsList userId={userId} />
     </div>
-  )
+  );
 }
+
 
 export default BookingsHistory
